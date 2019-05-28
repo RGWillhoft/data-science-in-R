3a17c8b2fff7b6eb775e7ebf9b37bc6ffc0c6ff2
Robert Willhoft <willhoft_robert@roberts.edu>
2019-05-27 20:24:31 -0400

    Chapter 12 and 13 done

--- chapter-12-exercises/exercise-12-1/exercise-12-1.R
+++ chapter-12-exercises/exercise-12-1/exercise-12-1.R
@@ -1,4 +1,5 @@
 # Exercise 1: analyzing avocado sales with the `tidyr` package
+# RG Willhoft, 2019
 
 # Load necessary packages (`tidyr`, `dplyr`, and `ggplot2`)
 # install.packages( "tidyr")
@@ -25,15 +26,19 @@ avocados <- mutate( avocados, Date = as.Date(Date) )
 # `X4046` to `small_haas`
 # `X4225` to `large_haas`
 # `X4770` to `xlarge_haas`
-
+# colnames(avocados)[colnames(avocados)=='X4046'] <- 'small_haas'
+# colnames(avocados)[colnames(avocados)=='X4225'] <- 'large_haas'
+# colnames(avocados)[colnames(avocados)=='X4770'] <- 'xlarge_haas'
+avocados <- avocados %>% 
+  rename(small_haas = X4046, large_haas = X4225, xlarge_haas = X4770)
 
 # The data only has sales for haas avocados. Create a new column `other_avos`
 # that is the Total.Volume minus all haas avocados (small, large, xlarge)
-
+avocados <- mutate( avocados, other_avos = Total.Volume - (small_haas+large_haas+xlarge_haas))
 
 # To perform analysis by avocado size, create a dataframe `by_size` that has
 # only `Date`, `other_avos`, `small_haas`, `large_haas`, `xlarge_haas`
-
+by_size = select( avocados, Date, other_avos, small_haas, large_haas, xlarge_haas )
 
 # In order to visualize this data, it needs to be reshaped. The four columns
 # `other_avos`, `small_haas`, `large_haas`, `xlarge_haas` need to be 
@@ -42,29 +47,33 @@ avocados <- mutate( avocados, Date = as.Date(Date) )
 # `volume`. Create a new dataframe `size_gathered` by passing the `by_size` 
 # data frame to the `gather()` function. `size_gathered` will only have 3 
 # columns: `Date`, `size`, and `volume`.
-
+size_gathered <- gather( by_size, key = size, value = volume, -Date )
 
 # Using `size_gathered`, compute the average sales volume of each size 
 # (hint, first `group_by` size, then compute using `summarize`)
-
+average_sales <- size_gathered %>%
+  group_by(size) %>%
+  summarize(mean(volume))
+average_sales
 
 # This shape also facilitates the visualization of sales over time
 # (how to write this code is covered in Chapter 16)
 ggplot(size_gathered) +
-  geom_smooth(mapping = aes(x = Date, y = volume, col = size), se = F) 
-
+  geom_smooth(mapping = aes(x = Date, y = volume, col = size), se = F) # error, don't know why
 
 # We can also investigate sales by avocado type (conventional, organic).
 # Create a new data frame `by_type` by grouping the `avocados` dataframe by
 # `Date` and `type`, and calculating the sum of the `Total.Volume` for that type
 # in that week (resulting in a data frame with 2 rows per week).
-
+by_type <- avocados %>%
+  group_by(Date,type) %>%
+  summarize(week_total = sum(Total.Volume))
 
 # To make a (visual) comparison of conventional versus organic sales, you 
 # need to **spread** out the `type` column into two different columns. Create a 
 # new data frame `by_type_wide` by passing the `by_type` data frame to 
 # the `spread()` function!
-
+by_type_wide <- spread(by_type,key=type,value=week_total)
 
 # Now you can create a scatterplot comparing conventional to organic sales!
 # (how to write this code is covered in Chapter 16)
--- chapter-13-exercises/exercise-13-1/exercise-13-1.R
+++ chapter-13-exercises/exercise-13-1/exercise-13-1.R
@@ -2,34 +2,39 @@
 
 # Install and load the `dplyr`, `DBI`, and `RSQLite` packages for accessing
 # databases
-
+# install.packages("dbplyr")
+library("dplyr")
+library("DBI")
+# install.packages("RSQLite")
+library("RSQLite")
 
 # Create a connection to the `Chinook_Sqlite.sqlite` file in the `data` folder
 # Be sure to set your working directory!
-
+chinook_sqlite <- dbConnect(SQLite(), dbname="data/chinook_sqlite.sqlite")
 
 # Use the `dbListTables()` function (passing in the connection) to get a list
 # of tables in the database.
-
+tables <- dbListTables(chinook_sqlite)
 
 # Use the `tbl()`function to create a reference to the table of music genres.
 # Print out the the table to confirm that you've accessed it.
-
+genre_table <- tbl(chinook_sqlite,"Genre")
 
 # Try to use `View()` to see the contents of the table. What happened?
-
+View(genre_table)
 
 # Use the `collect()` function to actually load the genre table into memory
 # as a data frame. View that data frame.
-
+genre_df <- collect(genre_table)
+View(genre_df)
 
 # Use dplyr's `count()` function to see how many rows are in the genre table
-
-
+count(genre_df)
 
 # Use the `tbl()` function to create a reference the table with track data.
 # Print out the the table to confirm that you've accessed it.
-
+track_data_table <- tbl(chinook_sqlite,"Track")
+print(track_data_table)
 
 # Use dplyr functions to query for a list of artists in descending order by
 # popularity in the database (e.g., the artist with the most tracks at the top)
@@ -37,14 +42,25 @@
 #   group rows by the artist and count them. Finally, arrange the results.
 # - Use pipes to do this all as one statement without collecting the data into
 #   memory!
-
+artist_query <- track_data_table %>%
+  filter( !is.na(composer) ) %>%
+  group_by( composer ) %>%
+  count() %>%
+  arrange(-n)
+artist_query_data <- collect( artist_query )
 
 # Use dplyr functions to query for the most popular _genre_ in the library.
 # You will need to count the number of occurrences of each genre, and join the
 # two tables together in order to also access the genre name.
 # Collect the resulting data into memory in order to access the specific row of
 # interest
-
+popular_genre <- track_data_table %>%
+  group_by(GenreId) %>%
+  count() %>%
+  arrange(-n) %>%
+  left_join( genre_df, copy = TRUE )
+most_popular_genre <- collect(popular_genre)$Name[1]
+most_popular_genre
 
 # Bonus: Query for a list of the most popular artist for each genre in the
 # library (a "representative" artist for each).
@@ -53,3 +69,4 @@
 
 
 # Remember to disconnect from the database once you are done with it!
+dbDisconnect( chinook_sqlite )
