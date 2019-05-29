5b49761a571a55f63a25a9b9b0ae26d88f4796fa
Robert Willhoft <willhoft_robert@roberts.edu>
2019-05-29 13:30:18 -0400

    Completed exercises for chapter 14

--- chapter-14-exercises/exercise-14-1/exercise-14-1.R
+++ chapter-14-exercises/exercise-14-1/exercise-14-1.R
@@ -1,55 +1,64 @@
 # Exercise 1: reading and querying a web API
+# RG Willhoft, 2019
 
 # Load the httr and jsonlite libraries for accessing data
 # You can also load `dplyr` if you wish to use it
-
+library("httr")
+library("jsonlite")
+library("dplyr")
 
 # Create a variable base_uri that stores the base URI (as a string) for the 
 # Github API (https://api.github.com)
+base_url <- "https://api.github.com"
+
+# RGW Hint for next step:
+#   enter https://api.gethub.com to get documentation
 
 
 # Under the "Repositories" category of the API documentation, find the endpoint 
 # that will list _repos in an organization_. Then create a variable named
 # `org_resource` that stores the endpoint for the `programming-for-data-science`
 # organization repos (this is the _path_ to the resource of interest).
-
+org_resource <- "/orgs/programming-for-data-science/repos" # {?type,page,per_page,sort}"'
 
 # Send a GET request to this endpoint (the `base_uri` followed by the 
 # `org_resource` path). Print the response to show that your request worked. 
 # (The listed URI will also allow you to inspect the JSON in the browser easily).
-
+response <- GET( paste0(base_url,org_resource) )
 
 # Extract the content of the response using the `content()` function, saving it
 # in a variable.
-
+response_text <- content(response, "text")
 
 # Convert the content variable from a JSON string into a data frame.
-
+repositories_data <- fromJSON(response_text)
 
 # How many (public) repositories does the organization have?
-
+nrow( repositories_data )
 
 # Now a second query:
 # Create a variable `search_endpoint` that stores the endpoint used to search 
 # for repositories. (Hint: look for a "Search" endpoint in the documentation).
-
+search_endpoint <- "/search/repositories" # ?q={query}{&page,per_page,sort,order}","
 
 # Search queries require a query parameter (for what to search for). Create a 
 # `query_params` list variable that specifies an appropriate key and value for 
 # the search term (you can search for anything you want!)
-
+query_parms <- list(q="exercise-14")
 
 # Send a GET request to the `search_endpoint`--including your params list as the
 # `query`. Print the response to show that your request worked.
-
+search_result <- GET( paste0(base_url,search_endpoint), query=query_parms)
+search_result_text <- content( search_result, type="text" )
 
 # Extract the content of the response and convert it from a JSON string into a
 # data frame. 
-
+search_result_data <- fromJSON(search_result_text)
+search_items <- search_result_data$items
 
 # How many search repos did your search find? (Hint: check the list names to 
 # find an appropriate value).
-
+search_result_data$total_count
 
 # What are the full names of the top 5 repos in the search results?
-
+print( search_items$full_name[1:5] )
--- chapter-14-exercises/exercise-14-2/exercise-14-2.R
+++ chapter-14-exercises/exercise-14-2/exercise-14-2.R
@@ -1,18 +1,20 @@
 # Exercise 2: working with data APIs
+# RG Willhoft, 2019
 
 # load relevant libraries
 library("httr")
 library("jsonlite")
 
 # Be sure and check the README.md for complete instructions!
+# https://developer.nytimes.com/accounts/create
 
 
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source( "appkeys.R" )
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+movie_name <- "The Hunt for Red October"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -23,10 +25,14 @@ library("jsonlite")
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
 
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_params <- list(query = "the hunt for red october", "api-key" = nytimes_key )
+# https://api.nytimes.com/svc/movies/v2/reviews/search.json?query=godfather&api-key=yourkey
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response_data <- fromJSON( content( GET( paste0(base_uri,resource),query=query_params), "text" ) )
 
 # What kind of data structure did this produce? A data frame? A list?
 
@@ -34,14 +40,18 @@ library("jsonlite")
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
-
+results <- response_data$results
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten( results )
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+headline <- reviews$headline[1]
+short_summary <- reviews$summary_short[1]
+link <- reviews$link.url[1]
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
+movie_info <- list( headline = headline, summary = short_summary, link = link )
+print( movie_info )
