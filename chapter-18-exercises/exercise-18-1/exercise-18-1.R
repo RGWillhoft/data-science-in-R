a081fccf02797fda7e007fbc691593e553c1b960
Robert Willhoft <willhoft_robert@roberts.edu>
2019-06-10 16:40:16 -0400

    Chapter 19 solutions

--- chapter-18-exercises/exercise-18-1/exercise-18-1.R
+++ chapter-18-exercises/exercise-18-1/exercise-18-1.R
@@ -4,13 +4,12 @@ library("jsonlite")
 
 # Be sure and check the README.md for complete instructions!
 
-
 # Use `source()` to load your API key variable from the `apikey.R` file you made.
 # Make sure you've set your working directory!
-
+source( "../../../api_keys.R" )
 
 # Create a variable `movie_name` that is the name of a movie of your choice.
-
+# movie_name <- "Hidden Figures"
 
 # Construct an HTTP request to search for reviews for the given movie.
 # The base URI is `https://api.nytimes.com/svc/movies/v2/`
@@ -20,11 +19,13 @@ library("jsonlite")
 #
 # You should use YOUR api key (as the `api-key` parameter)
 # and your `movie_name` variable as the search query!
-
+base_uri <- "https://api.nytimes.com/svc/movies/v2/"
+resource <- "reviews/search.json"
+query_params <- list(query = movie_name, "api-key" = nytimes_key )
 
 # Send the HTTP Request to download the data
 # Extract the content and convert it from JSON
-
+response_data <- fromJSON( content( GET( paste0(base_uri,resource),query=query_params), "text" ) )
 
 # What kind of data structure did this produce? A data frame? A list?
 
@@ -32,14 +33,19 @@ library("jsonlite")
 # Manually inspect the returned data and identify the content of interest 
 # (which are the movie reviews).
 # Use functions such as `names()`, `str()`, etc.
+results <- response_data$results
 
 
 # Flatten the movie reviews content into a data structure called `reviews`
-
+reviews <- flatten( results )
 
 # From the most recent review, store the headline, short summary, and link to
 # the full article, each in their own variables
-
+review_headline <- reviews$headline[1]
+review_summary <- reviews$summary_short[1]
+review_link_text <- reviews$link.suggested_link_text[1]
+review_link <- reviews$link.url[1]
+review_date <- reviews$publication_date[1]
 
 # Create a list of the three pieces of information from above. 
 # Print out the list.
